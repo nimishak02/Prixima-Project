70ed842e88043dfad1cc68c79d7d89b576471794
nimishak02 <nimishacr02@gmail.com>
2023-05-23 09:29:48 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    // HERO-SLIDER
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        smartSpeed:1000,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false,
+        navText:["PREV" ,"NEXT"],
+        responsive:{
+            0:{
+                nav:false,
+            },
+            768:{
+                nav:true,
+            }
+        }
+    });
+    // PROJECT-SLIDER
+    $('#project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        smartSpeed:1000,
+        dots:false,
+        navText:["PREV" ,"NEXT"],
+        responsive:{
+            0:{
+               items:1,
+               nav:false,
+               margin:0,
+            },
+            768:{
+                items:2,
+            },
+            1140:{
+                   items:2,
+                   center:2,
+            }
+        }
+    })
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:1,
+        smartSpeed:700,
+    })
+})
